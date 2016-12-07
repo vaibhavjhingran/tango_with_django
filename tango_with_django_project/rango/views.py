779482e7eb8bf3ca9cf67329cac96335adf89867
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-12-07 23:18:24 +0530

    Adding static + template settings & files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says: Yo! <br/>Also, Do check out our <a href='/about'>about</a> page.")
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie dough..."}
+	return render(request, 'rango/index.html', context=context_dict)
+	# return HttpResponse("Rango says: Yo! <br/>Also, Do check out our <a href='/about'>about</a> page.")
 
 def about(request):
 	return HttpResponse("Rango says: this be the about page! <br/>Or go to the <a href='/rango'>Home Page</a>")

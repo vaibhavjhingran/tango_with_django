1b81a9048c4d9a7f4a705f8fdc6b560392381ccb
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-12-09 22:37:28 +0530

    Session + cookie + cookie setting helper function

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -10,16 +10,36 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserProfileForm
 from rango.forms import UserForm
+from datetime import datetime
+
+# Helper function
+def visitor_cookie_handler(request, response):
+	visits = int(request.COOKIES.get('visits', '1'))
+	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		response.set_cookie('last_visit', str(datetime.now()))
+	else:
+		response.set_cookie('last_visit', last_visit_cookie)
+
+	response.set_cookie('visits', visits)
 
 def index(request):
+	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': pages_list}
-	return render(request, 'rango/index.html', context=context_dict)
-	# context_dict = {'boldmessage': "Crunchy, creamy, cookie dough..."}
-	# return render(request, 'rango/index.html', context=context_dict)
+	response = render(request, 'rango/index.html', context=context_dict)
+
+	visitor_cookie_handler(request, response)
+	return response
 
 def about(request):
+	if request.session.test_cookie_worked():
+		print "TEST COOKIE WORKING!!!"
+		request.session.delete_test_cookie()
 	context_dict = {'aboutmessage': "This is about us right here buddy!", 'creator': "Created by iceman!"}
 	return render(request, 'rango/about.html', context=context_dict)
 
@@ -127,3 +147,4 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
+

d874d9be37062a85b9fa6f8057ada577e8add68a
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-12-09 17:47:18 +0530

    Adding initial auth related work + User Profile model work

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
 	name = models.CharField(max_length=55, unique=True)
@@ -26,3 +27,17 @@ class Page(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+class UserProfile(models.Model):
+	# Liks to User model instance
+	user = models.OneToOneField(User)
+
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __unicode__(self):
+		return self.user.username
+
+	# Needed?
+	# def __str__(self):
+	# 	return self.user.username
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -97,6 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -105,6 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Password Hashing
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 # Internationalization

68886c4a7a77ee6646973ec972a5d29295d9e702
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-12-09 23:35:10 +0530

    Changing to server side cookie handling.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -12,28 +12,54 @@ from rango.forms import UserProfileForm
 from rango.forms import UserForm
 from datetime import datetime
 
-# Helper function
-def visitor_cookie_handler(request, response):
-	visits = int(request.COOKIES.get('visits', '1'))
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+# Helper function for client side cookie collection
+# def visitor_cookie_handler(request, response):
+# 	visits = int(request.COOKIES.get('visits', '1'))
+# 	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+# 	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+# 	if (datetime.now() - last_visit_time).days > 0:
+# 		visits = visits + 1
+# 		response.set_cookie('last_visit', str(datetime.now()))
+# 	else:
+# 		response.set_cookie('last_visit', last_visit_cookie)
+
+# 	response.set_cookie('visits', visits)
+
+# Helper function to store server side session cookie
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
 	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
 	if (datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
-		response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
 
 def index(request):
 	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': pages_list}
+	
+	# In case of server side cookie handling
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
 	response = render(request, 'rango/index.html', context=context_dict)
 
-	visitor_cookie_handler(request, response)
+	# This is used for client side cookie handling 
+	# visitor_cookie_handler(request, response)
 	return response
 
 def about(request):
